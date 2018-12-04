290bd60bea95af86394854107dbae258f7a7795d
liwei <wei.li@zenjoy.net>
2018-12-04 10:08:07 +0800

    Modify musics data4

--- todo/app/src/main/java/com/k/todo/HomeActivity.java
+++ todo/app/src/main/java/com/k/todo/HomeActivity.java
@@ -1,24 +1,66 @@
 package com.k.todo;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 import com.k.todo.base.DataProvider;
+import com.k.todo.base.MusicServiceEventListener;
 import com.k.todo.base.PermissionActivity;
 import com.k.todo.fragments.MainFragment;
 import com.k.todo.provider.DataSourceListener;
 import com.k.todo.provider.MusicProvider;
+import com.k.todo.service.MusicPlayerRemote;
 
-public class HomeActivity extends PermissionActivity implements DataSourceListener, DataProvider {
+import java.util.ArrayList;
+
+public class HomeActivity extends PermissionActivity implements DataSourceListener, DataProvider, MusicServiceEventListener {
     private MusicProvider provider;
     private MainFragment mainFragment;
+    private final ArrayList<MusicServiceEventListener> mMusicServiceEventListeners = new ArrayList<>();
+
+    private MusicPlayerRemote.ServiceToken serviceToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        serviceToken = MusicPlayerRemote.bindToService(this, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                HomeActivity.this.onServiceConnected();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                HomeActivity.this.onServiceDisconnected();
+            }
+        });
+
         provider = MusicProvider.create();
         mainFragment = MainFragment.instance();
         getSupportFragmentManager().beginTransaction().replace(R.id.home_container, mainFragment).commitAllowingStateLoss();
+    }
+
+
+    public void addMusicServiceEventListener(final MusicServiceEventListener listener) {
+        if (listener != null) {
+            mMusicServiceEventListeners.add(listener);
+        }
+    }
+
+    public void removeMusicServiceEventListener(final MusicServiceEventListener listener) {
+        if (listener != null) {
+            mMusicServiceEventListeners.remove(listener);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MusicPlayerRemote.unbindFromService(serviceToken);
+
     }
 
     @Override
@@ -45,5 +87,45 @@ public class HomeActivity extends PermissionActivity implements DataSourceListen
     @Override
     public MusicProvider getMusicProvider() {
         return provider;
+    }
+
+    @Override
+    public void onServiceConnected() {
+
+    }
+
+    @Override
+    public void onServiceDisconnected() {
+
+    }
+
+    @Override
+    public void onQueueChanged() {
+
+    }
+
+    @Override
+    public void onPlayingMetaChanged() {
+
+    }
+
+    @Override
+    public void onPlayStateChanged() {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged() {
+
+    }
+
+    @Override
+    public void onShuffleModeChanged() {
+
+    }
+
+    @Override
+    public void onMediaStoreChanged() {
+
     }
 }
---
+++ todo/app/src/main/java/com/k/todo/base/MusicServiceEventListener.java
@@ -0,0 +1,20 @@
+package com.k.todo.base;
+
+
+public interface MusicServiceEventListener {
+    void onServiceConnected();
+
+    void onServiceDisconnected();
+
+    void onQueueChanged();
+
+    void onPlayingMetaChanged();
+
+    void onPlayStateChanged();
+
+    void onRepeatModeChanged();
+
+    void onShuffleModeChanged();
+
+    void onMediaStoreChanged();
+}
