1010386c6e6961e2c9a309180b78dea823b8d7c0
liwei <wei.li@zenjoy.net>
2021-06-23 21:01:08 +0800

    [LW] add player module

--- todo/app/src/main/java/com/k/todo/base/OnItemClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.k.todo.base
-
-
-import android.view.View
-
-interface OnItemClickListener {
-
-    fun onItemClick(itemView: View, position: Int, `object`: Any)
-}
--- todo/app/src/main/java/com/k/todo/util/AsyLoader.kt
+++
@@ -1,6 +0,0 @@
-package com.k.todo.util
-
-
-class AsyLoader{
-
-}
